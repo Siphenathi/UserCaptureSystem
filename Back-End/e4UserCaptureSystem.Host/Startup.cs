faf69c4a589e655e777ff95b68ef7744c222ddbd
Siphenathi <spantshwa.lukho@gmail.com>
2023-02-24 16:26:05 +0200

    Refactored code

--- Back-End/e4UserCaptureSystem.Host/Startup.cs
+++ Back-End/e4UserCaptureSystem.Host/Startup.cs
@@ -1,17 +1,11 @@
 using e4UserCaptureSystem.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.OpenApi.Models;
 
 namespace e4UserCaptureSystem.Host
@@ -44,33 +38,6 @@ namespace e4UserCaptureSystem.Host
 					Title = "UserCaptureSystem v1",
 					Description = "UserCaptureSystem v1",
 				});
-
-				// To Enable authorization using Swagger (JWT) 
-				//c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-				//{
-				//	Name = "Authorization",
-				//	Type = SecuritySchemeType.ApiKey,
-				//	Scheme = "Bearer",
-				//	BearerFormat = "JWT",
-				//	In = ParameterLocation.Header,
-				//	Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\""
-				//});
-
-				//c.AddSecurityRequirement(new OpenApiSecurityRequirement
-				//{
-				//	{
-				//		new OpenApiSecurityScheme
-				//		{
-				//			Reference = new OpenApiReference
-				//			{
-				//				Type = ReferenceType.SecurityScheme,
-				//				Id = "Bearer"
-				//			}
-				//		},
-				//		new string[] {}
-				//	}
-				//});
-
 			});
 			services.AddScoped<IUserService>(serviceProvider => new UserService(GetFilePath()));
 		}
